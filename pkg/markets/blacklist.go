578604c577f4250f9b81cdd4c611d92f773790b5
Jorge Olivero <jorgeaolivero@gmail.com>
2018-07-20 21:02:48 -0500

    add to blacklist

--- pkg/markets/blacklist.go
+++ pkg/markets/blacklist.go
@@ -2,6 +2,7 @@ package markets
 
 var blacklist = map[string]struct{}{
 	"0x4c2b2e090c05987f3ae9d95745ebb7d127b4e627": struct{}{},
-	"0xdd6eb36fd81a753911073ce3a33c244563540a53":   struct{}{},
+	"0xdd6eb36fd81a753911073ce3a33c244563540a53": struct{}{},
 	"0xde2ed6ac15b56e15addde6b77f3cc265a98fad54": struct{}{},
+	"0xf39b19fa684f5e09af8931a44276fd7a4aa8db6d": struct{}{},
 }
