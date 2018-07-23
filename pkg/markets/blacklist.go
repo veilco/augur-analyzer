dd76c0e7711d44d4f68f5fdfb37291cb57a2a075
Jorge Olivero <jorgeaolivero@gmail.com>
2018-07-23 15:40:45 -0500

    blacklist mass shooting market

--- pkg/markets/blacklist.go
+++ pkg/markets/blacklist.go
@@ -5,4 +5,5 @@ var blacklist = map[string]struct{}{
 	"0xdd6eb36fd81a753911073ce3a33c244563540a53": struct{}{},
 	"0xde2ed6ac15b56e15addde6b77f3cc265a98fad54": struct{}{},
 	"0xf39b19fa684f5e09af8931a44276fd7a4aa8db6d": struct{}{},
+	"0x62b9132b5022dd5fec055233c12d7d4915963570": struct{}{},
 }
