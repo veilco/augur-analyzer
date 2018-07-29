d9fe74709fa5cdcbecab348f39e300640478e806
Jorge Olivero <jorgeaolivero@gmail.com>
2018-07-29 09:50:24 -0500

    more blacklisted markets

--- pkg/markets/blacklist.go
+++ pkg/markets/blacklist.go
@@ -7,4 +7,7 @@ var blacklist = map[string]struct{}{
 	"0xf39b19fa684f5e09af8931a44276fd7a4aa8db6d": struct{}{},
 	"0x62b9132b5022dd5fec055233c12d7d4915963570": struct{}{},
 	"0x59079569d48530135ea7b032f2297b616439ebd7": struct{}{},
+	"0x6307c571135429576c8480acf2659780b898ab58": struct{}{},
+	"0x02d59d318167d6aab9be0ece52d71474d2990f5e": struct{}{},
+	"0x2f2f1beeedfa69617e1c35b8efadd7c22bab2ceb": struct{}{},
 }
